9feb67dbeaa8fcb2d6ca578ea5e85eeb5f1517d0
Oleksandr Parukha <oparu@softserveinc.com>
2022-07-28 14:41:20 +0200

    adds ef packages and DataContext

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using dotnet5_webapp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet5_webapp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
