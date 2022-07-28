a2f77184df5a626cbb4970b2c14b6223cfbcfb7b
Oleksandr Parukha <oparu@softserveinc.com>
2022-07-28 13:12:52 +0200

    adds new controller ContactController

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using dotnet5_webapp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet5_webapp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : Controller
+    {
+        private List<Contact> _contacts = new List<Contact>()
+        {
+            new Contact {Id = 1, FirstName = "Dal", LastName = "Ci", NickName = "Cut", Place = "NY"},
+            new Contact {Id = 2, FirstName = "Dal0", LastName = "Ci0", NickName = "Cut0", Place = "NY0"},
+        };
+        [HttpGet]
+        public IEnumerable<Contact> Get()
+        {
+            return _contacts;
+        }
+    }
+}
