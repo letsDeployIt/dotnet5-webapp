375d7214b1b1d3f23afeee214c99508c690d4936
Oleksandr Parukha <oparu@softserveinc.com>
2022-07-28 12:55:09 +0200

    adds new model Contact

---
+++ Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace dotnet5_webapp.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NickName { get; set; }
+        public string Place { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsCreated { get; set; }
+    }
+}
